6cb12d7384637f98909b7843a580c30bab219307
bharath <bharath@192.168.137.101>
2022-10-31 10:31:06 +0530

    samco

---
+++ tharani/src/tharani/helloworld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package tharani;
+
+/**
+ * @author bharath
+ *
+ */
+public class helloworld {
+
+	/**
+	 * 
+	 */
+	public helloworld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	 System.out.println("hello world");
+	}
+
+}
