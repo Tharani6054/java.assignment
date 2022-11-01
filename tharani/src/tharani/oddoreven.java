a3611e402db0696d5f12dfd0654517eb8ed3328c
bharath <bharath@LAPTOP-9395K1P5>
2022-11-01 11:19:56 +0530

    odd or ev

---
+++ tharani/src/tharani/oddoreven.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package tharani;
+
+/**
+ * @author bharath
+ *
+ */
+public class oddoreven {
+
+	/**
+	 * 
+	 */
+	public oddoreven() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		      
+		        int num = 10;
+		 
+		        if (num % 2 == 0) {
+		 
+		            System.out.println(" Even number");
+		        }
+		 
+		        else {
+		 
+		            System.out.println( " Odd number");
+		        }
+	}
+
+}
