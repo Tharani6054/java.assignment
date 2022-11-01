5dd759d013548b59b1d121e9811ac9ba81549880
bharath <bharath@LAPTOP-9395K1P5>
2022-11-01 11:33:40 +0530

    fibbannocci

---
+++ tharani/src/tharani/fibbannocci.java
@@ -0,0 +1,25 @@
+package tharani;
+
+public class fibbannocci {
+
+	public fibbannocci() {
+		// TODO Auto-generated constructor stub
+	}
+
+public static void main(String[] args)
+{
+  
+    int num = 10;
+
+    if (num % 2 == 0) {
+
+        System.out.println(" Even number");
+    }
+
+    else {
+
+        System.out.println( " Odd number");
+    }
+}
+}
+
